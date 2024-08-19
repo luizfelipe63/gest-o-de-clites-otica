2a267b6cc3e9b9c47596280c808887cd0907f846
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2024-08-19 15:51:22 -0300

    feat: Creating a User

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
-import express from 'express'
+import express, { Request, request, Response, response } from 'express'
 import {errorHandler} from './middleware/errorHandler'
-
+import { PrismaClient } from '@prisma/client'
 
 export const app = express()
 
-app.use(errorHandler as any)
+const prisma = new PrismaClient()
 
+app.use(errorHandler as any)
+app.use(express.json());
+
+app.post('/', async (req, res) => {
+    
+    const { email, name } = req.body
+
+    const post = await prisma.user.create({
+        data:{
+            email,
+            name
+        }
+    })
+
+    return res
+    .status(201)
+    .json({data: {post}});
+  })
